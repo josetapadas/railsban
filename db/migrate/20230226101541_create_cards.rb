f4d5a59ea699976dd04f0e1ba72ea041af52068d
jose <jose.tapadas@gmail.com>
2023-02-26 10:32:08 +0000

    create the cards model and view

---
+++ db/migrate/20230226101541_create_cards.rb
@@ -0,0 +1,10 @@
+class CreateCards < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cards do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_26_101541) do
+  create_table "cards", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
